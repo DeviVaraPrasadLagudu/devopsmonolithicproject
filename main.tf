1147a9b36c2a9ac804c2d78a1d5732063bb524cb
DeviVaraPrasadLagudu <devivaraprasadlagudu@gmail.com>
2025-11-24 03:33:46 +0530

    Change instance type from t3.micro to t2.micro

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_launch_template" "web_server_as" {
     name = "myproject"
     image_id           = "ami-0cae6d6fe6048ca2c"
     vpc_security_group_ids = [aws_security_group.web_server.id]
-    instance_type = "t3.micro"
+    instance_type = "t2.micro"
     key_name = "minimajor"
     tags = {
         Name = "DevOps"
